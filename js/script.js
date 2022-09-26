04c539b53f20626e675703f255bc499d8c5ea9ab
luigicr95 <112467906+luigicr95@users.noreply.github.com>
2022-09-26 16:39:07 +0200

    yet another try

--- js/script.js
+++ js/script.js
@@ -10,6 +10,8 @@ getDays(1, 2022);
 function todaysDate() {
   let today = new Date();
   let days = today.getDate();
-  let months = today.getMonth();
+  let months = today.getMonth() + 1;
   let years = today.getFullYear();
+  let actualDate = days + "/" + months + "/" + years;
+  document.getElementById("date").value = actualDate;
 }
